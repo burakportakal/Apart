8a93cb501a8b1e008fa1185ade76194e1a5e719e
Kadir Kanmaz <-->
2017-04-21 21:49:21 +0300

    Deneme sınıfı eklendi.

--- ApartYonetim/frmMusteriler.cs
+++ ApartYonetim/frmMusteriler.cs
@@ -37,7 +37,7 @@ namespace ApartYonetim
       "[daire_no] as 'Daire No'" +
       ",[kira_odemeTarihi] as 'Ödeme Tarihi'" +
       ",[kira_durumu] as 'Kira Durumu'" +
-  "FROM[AYS].[dbo].[tbl_Kira]", con);
+      "FROM[AYS].[dbo].[tbl_Kira]", con);
             ds = new DataSet();
             con.Open();
             da.Fill(ds, "tbl_Kira");
@@ -69,69 +69,10 @@ namespace ApartYonetim
             }
 
             gridView1.ClearSelection();
-
-
-
-            //if (e.RowHandle >= 0)
-            //{
-            //    string category = View.GetRowCellDisplayText(e.RowHandle, View.Columns["Daire No"]);
-            //    if (category == "16")
-            //    {
-            //        e.Appearance.BackColor = Color.Yellow;
-            //        e.Appearance.BackColor2 = Color.SeaShell;
-            //    }
-            //}
-
+   
         }
     }
 }
-
-        //private void gridView1_RowStyle(object sender, DevExpress.XtraGrid.Views.Grid.RowStyleEventArgs e)
-        //{
-        //    GridView View = sender as GridView;
-        //    if (e.RowHandle >= 0)
-        //    {
-        //        if (View.GetRowCellDisplayText(e.RowHandle, View.Columns["[musteri_adiSoyadi]"]) != "")
-        //        {
-        //            //int category = Convert.ToInt32(View.GetRowCellDisplayText(e.RowHandle, View.Columns["PersonelId"]));
-
-        //            {
-
-        //                e.Appearance.BackColor = Color.Salmon;
-        //                e.Appearance.BackColor2 = Color.SeaShell;
-        //            }
-        //        }
-        //        else
-        //        {
-        //            e.Appearance.BackColor = Color.WhiteSmoke;
-        //            e.Appearance.BackColor2 = Color.Cyan;
-        //        }
-
-        //    }
-
-        //}
-
-
-
-        //private void gridView1_RowStyle(object sender, RowStyleEventArgs e)
-        //{
-        //    GridView View = sender as GridView;
-        //    if (e.RowHandle >= 0)
-        //    {
-        //        string okunma = View.GetRowCellDisplayText(e.RowHandle, View.Columns[0]);
-        //        if (okunma == "Bos")
-        //        {
-        //            e.Appearance.BackColor = Color.Red;
-
-        //        }
-        //        else
-        //        {
-        //            e.Appearance.BackColor = Color.Purple;
-
-        //        }
-
-        //    }
-        //}
 
        
 
