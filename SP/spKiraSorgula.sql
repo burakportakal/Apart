441b670338173297a462f66d9473768331d734ef
buraqpo <buraqportakal@hotmail.com>
2017-04-23 22:01:53 +0300

    müşteri sorgulama yönetimi değişti

--- SP/spKiraSorgula.sql
+++ SP/spKiraSorgula.sql
@@ -13,33 +13,32 @@ GO
 -- Create date: 23/04/2017
 -- Description:	Kiraları sorgular ekranda ödemeyenler kırmızı ödeyenler mavi görünür.Daha sonra kapsamı genişletilecek.
 -- =============================================
-CREATE PROCEDURE [dbo].[spKiraSorgula]
+ALTER PROCEDURE [dbo].[spKiraSorgula](@kiraDonemi varchar(50),@kiraDurumu bit,@binaAdi nvarchar(50),@yetkili bit,@daireNo int)
 AS
 BEGIN
-SELECT        tbl_Musteriler.musteri_tc_kimlik_no as 'TC Kimlik No',
+SELECT        
+			  tbl_Kiralar.kira_donemi as 'Kira Dönemi',
+			  tbl_Musteriler.musteri_tc_kimlik_no as 'TC Kimlik No',
 			  tbl_Musteriler.musteri_adi as 'Adı',
 			  tbl_Musteriler.musteri_soyadi as 'Soyadı',
 			  tbl_Musteriler.musteri_telefon_no as 'Telefon',
-			  tbl_Musteriler.musteri_telefon_no2 as 'Telefon 2',
 			  tbl_Musteriler.musteri_email as 'Email', 
-			  tbl_Musteriler.musteri_sehir as 'Şehir',
-			  tbl_Musteriler.musteri_adres as 'Adres',
 			  tbl_Daireler.daire_kapi_no as 'Daire Kapı No',
-		   	  tbl_Binalar.bina_adi as 'Apart Adı',
+		  	  tbl_Binalar.bina_adi as 'Apart Adı',
 			  tbl_Kiralar.kira_durumu as 'Kira Durumu',
-			  tbl_Musteriler.musteri_aciklama as 'Açıklama',
-			  tbl_Musteriler.musteri_kontrat_baslangic_tarihi as 'Kontrat Başlangıç',
-			  tbl_Musteriler.musteri_kontrat_bitis_tarihi as 'Kontrat Bitiş', 
 			  tbl_Musteriler.musteri_kira_tutari as 'Kira Miktarı',
-			  tbl_Musteriler.musteri_yetki as 'Yetkili',
-			  tbl_Musteriler.musteri_durumu as 'Durumu'
+			  tbl_Musteriler.musteri_yetki as 'Yetkili'
 			  
-FROM            tbl_Musteriler INNER JOIN
-                         tbl_Daireler ON tbl_Musteriler.daire_no = tbl_Daireler.daire_no INNER JOIN
-                         tbl_Binalar ON tbl_Daireler.bina_id = tbl_Binalar.bina_id INNER JOIN
-						 tbl_Kiralar ON tbl_Kiralar.daire_no = tbl_Daireler.daire_no
+FROM            tbl_Kiralar INNER JOIN
+                         tbl_Daireler ON tbl_Kiralar.daire_no = tbl_Daireler.daire_no INNER JOIN
+						 tbl_Binalar ON tbl_Daireler.bina_id = tbl_Binalar.bina_id INNER JOIN
+                         tbl_Musteriler ON tbl_Kiralar.daire_no = tbl_Musteriler.daire_no
+Where
+			kira_donemi=CASE WHEN @kiraDonemi='' THEN kira_donemi ELSE @kiraDonemi END and musteri_yetki=1 and kira_durumu=@kiraDurumu and
+			bina_adi=CASE WHEN @binaAdi='' THEN bina_adi ELSE @binaAdi END and musteri_yetki= CASE WHEN @yetkili=1 THEN @yetkili ELSE musteri_yetki END
+				  and daire_kapi_no= CASE WHEN @daireNo=0 THEN daire_kapi_no ELSE @daireNo END 
+Order by kira_donemi
 END
 
-GO
 
 
